a6b2ca2d99fe1aebf3456eab1b99cf65aef2370b
Murali Anand <smurali1607@gmail.com>
2026-01-07 19:42:43 +0530

    Remove dist folder and its contents from version control

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { MagmaConnect } from './plugin';
-export type { MagmaConnectOptions } from './plugin';
--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MagmaConnect = void 0;
-var plugin_1 = require("./plugin");
-Object.defineProperty(exports, "MagmaConnect", { enumerable: true, get: function () { return plugin_1.MagmaConnect; } });
